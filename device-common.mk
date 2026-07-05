c060cc69ebe2e6b1c33ba8e3f92aa5c8d0b97f02
Vega <vega@pocox3pro.local>
2026-07-05 09:03:03 +0200

    Reapply "Aerodactyl: Inherit hardware/nothing"
    This reverts commit 9b69441983b331e7a3b7263f354e770042bbbd7f.

--- device-common.mk
+++ device-common.mk
@@ -230,7 +230,7 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_PACKAGES += \
-    NothingFelica
+    NothingFelicaDisabler
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.ese.xml \
@@ -243,11 +243,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_JPN/android.hardware.se.omapi.ese.xml
 
 # NTF
-PRODUCT_PACKAGES += \
-    nt-fwk.Aerodactyl
-
-PRODUCT_BOOT_JARS += \
-    nt-fwk.Aerodactyl
+$(call inherit-product, hardware/nothing/nt-fwk/nt-fwk.mk)
 
 # Overlays
 $(call inherit-product, hardware/mediatek/overlay/mssi.mk)
@@ -265,6 +261,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     LineageApertureResTarget \
+    LineageDozeResTarget \
     LineageSDKResTarget \
     LineageSettingsProviderResTarget \
     LineageSettingsResTarget \
@@ -334,6 +331,9 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.boot_security_patch=$(BOOT_SECURITY_PATCH)
 
 # Sensors
+$(call soong_config_set,nothing_sensors,tp_udfps_path,/sys/devices/platform/soc/11013000.spi3/spi_master/spi3/spi3.0/fts_gesture_fod_pressed)
+$(call soong_config_set,nothing_sensors,tp_single_tap_path,/sys/devices/platform/soc/11013000.spi3/spi_master/spi3/spi3.0/fts_gesture_single_tap_pressed)
+
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal \
     android.hardware.sensors@2.0-subhal-impl-1.0 \
@@ -358,7 +358,8 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \
-    hardware/mediatek/wlan/wifi_hal
+    hardware/mediatek/wlan/wifi_hal \
+    hardware/nothing
 
 # Task Profiles
 PRODUCT_COPY_FILES += \
