d59108777010feb1d48e71a55bbdcd8225c055fb
Mashopy <eliasgheeraert@gmail.com>
2026-07-07 07:13:38 +0200

    Aerodactyl: Switch to the prebuilt WiFi HAL
    Too lazy to finish the OSS one unfortunately

--- device-common.mk
+++ device-common.mk
@@ -367,7 +367,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \
-    hardware/mediatek/wlan/wifi_hal \
     hardware/nothing
 
 # Task Profiles
@@ -417,10 +416,15 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.software.opengles.deqp.level-2025-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
 
 # Wifi
+$(call soong_config_set_bool,mediatek_wifi_hal,use_pre_u_qpr2_struct,true)
+
 PRODUCT_PACKAGES += \
     wpa_supplicant \
     hostapd \
     android.hardware.wifi-service
+
+PRODUCT_PACKAGES += \
+    libwifi-hal-wrapper
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml \
